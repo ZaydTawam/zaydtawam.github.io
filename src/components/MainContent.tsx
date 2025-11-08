279dfe31a4d48e34218aec7bef609ad3db4271e2
Zayd Tawam <zt44794@gmail.com>
2025-11-08 15:27:52 -0500

    Update content

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,6 +1,6 @@
-import { RefObject, useEffect, useState } from 'react';
-import Section from './Section';
-import { Folders } from '@phosphor-icons/react';
+import { RefObject, useEffect, useState } from "react";
+import Section from "./Section";
+import { AsteriskSimple, Folders } from "@phosphor-icons/react";
 
 interface Props {
   sectionContainerRef: RefObject<HTMLDivElement>;
@@ -33,20 +33,20 @@ const MainContent = ({ sectionContainerRef, setActiveSection }: Props) => {
       const vw = window.innerWidth;
       if (sectionContainerRef.current) {
         if (sectionContainerRef.current.scrollLeft < 0.145 * vw) {
-          setActiveSection('about');
+          setActiveSection("about");
         } else if (sectionContainerRef.current.scrollLeft < 0.55 * vw) {
-          setActiveSection('projects');
+          setActiveSection("experience");
         } else if (sectionContainerRef.current.scrollLeft < 0.955 * vw) {
-          setActiveSection('skills');
+          setActiveSection("projects");
         } else {
-          setActiveSection('contact');
+          setActiveSection("contact");
         }
       }
     };
-    sectionContainerRef.current?.addEventListener('scroll', handleScroll);
+    sectionContainerRef.current?.addEventListener("scroll", handleScroll);
 
     return () => {
-      sectionContainerRef.current?.removeEventListener('scroll', handleScroll);
+      sectionContainerRef.current?.removeEventListener("scroll", handleScroll);
     };
   }, [sectionContainerRef]);
 
@@ -64,43 +64,70 @@ const MainContent = ({ sectionContainerRef, setActiveSection }: Props) => {
       onMouseMove={drag}
     >
       <Section sectionName="about">
-        I am a student at the University of Florida majoring in Computer
-        Science. My experience working on a diverse set of projects and the
-        courses I have taken have given me the technical skills to tackle any
-        challenge and solve complex problems.
+        I am a Computer Science student at the University of Florida with an
+        interest in full-stack development and machine learning. I enjoy
+        building impactful projects to solve real-world problems and create a
+        smooth user experience. I have gained hands on experience with
+        technologies like TypeScript, Python, and C++. As I grow as a developer,
+        I’m excited to keep exploring new avenues and grow my skills in software
+        engineering and AI/ML.
+      </Section>
+      <Section sectionName="experience">
+        <p style={{ marginBottom: "0.6vh" }}>
+          <AsteriskSimple size={13} /> UNDERGRADUATE RESEARCH ASSISTANT
+          <br />
+          <span style={{ color: " #9c9c9c" }}>
+            Artificial Intelligence Research Group
+          </span>
+          <br />
+          <span style={{ color: " #9c9c9c" }}>Oct 2025 - PRESENT</span>
+          <br />
+        </p>
+        <p style={{ marginBottom: "0.6vh" }}>
+          <AsteriskSimple size={13} /> TECHNICAL OFFICER
+          <br />
+          <span style={{ color: " #9c9c9c" }}>SOFTWARE ENGINEERING CLUB</span>
+          <br />
+          <span style={{ color: " #9c9c9c" }}>Aug 2025 - PRESENT</span>
+          <br />
+        </p>
+        <p style={{ marginBottom: "0.6vh" }}>
+          <AsteriskSimple size={13} /> TEAM MEMBER
+          <br />
+          <span style={{ color: " #9c9c9c" }}>
+            SHPE AUTONOMOUS VEHICLE TEAM
+          </span>
+          <br />
+          <span style={{ color: " #9c9c9c" }}>Aug 2025 - PRESENT</span>
+          <br />
+        </p>
       </Section>
       <Section sectionName="projects">
-        <p style={{ marginBottom: '0.6vh' }}>
-          <Folders size={13} color="black" /> DISCORD BOT
-          <span style={{ float: 'right', color: ' #9c9c9c' }}>Mar 2024</span>
+        <p style={{ marginBottom: "0.6vh" }}>
+          <Folders size={13} color="black" />{" "}
+          <a href="https://github.com/ZaydTawam/visa-coach">VISA COACH</a>
+          <span style={{ float: "right", color: " #9c9c9c" }}>
+            Jan 2025 - Jul 2025
+          </span>
         </p>
-        <p style={{ marginBottom: '0.6vh' }}>
+        <p style={{ marginBottom: "0.6vh" }}>
+          <Folders size={13} color="black" />{" "}
+          <a href="https://github.com/ZaydTawam/Orion-Discord-Bot">
+            DISCORD COMMUNITY BOT
+          </a>
+          <span style={{ float: "right", color: " #9c9c9c" }}>Feb 2024</span>
+        </p>
+        <p style={{ marginBottom: "0.6vh" }}>
           <Folders size={13} color="black" /> OTHELLO GAME
-          <span style={{ float: 'right', color: ' #9c9c9c' }}>Jul 2024</span>
+          <span style={{ float: "right", color: " #9c9c9c" }}>Jul 2024</span>
         </p>
         <p>
-          <Folders size={13} color="black" /> TASK MANAGER
-          <span style={{ float: 'right', color: ' #9c9c9c' }}>Nov 2023</span>
+          <Folders size={13} color="black" />{" "}
+          <a href="https://github.com/ZaydTawam/Task-Manager">TASK MANAGER</a>
+          <span style={{ float: "right", color: " #9c9c9c" }}>Nov 2023</span>
         </p>
       </Section>
-      <Section sectionName="skills">
-        Lorem ipsum odor amet, consectetuer adipiscing elit. Habitasse netus
-        mollis molestie arcu, netus magnis porta libero. Sodales mattis massa
-        elementum eros pulvinar adipiscing enim. Integer rhoncus ante curabitur
-        donec tortor nibh. Eu natoque himenaeos duis tristique massa tempor
-        elementum donec. Curae pretium laoreet, cubilia senectus tortor maximus.
-        Leo tempor varius bibendum ultrices; semper accumsan netus. Molestie
-        ridiculus fermentum, posuere vehicula fusce nam suscipit. Quis ultricies
-        felis bibendum sagittis lobortis hendrerit inceptos class.
-        <br />
-        <br />
-        Fermentum id ut montes consequat venenatis ac orci. Potenti semper
-        scelerisque elementum vehicula feugiat. Habitant rutrum fringilla diam
-        venenatis lacinia fringilla hendrerit. Est eu torquent sodales conubia
-        mus accumsan convallis in magnis. Mollis nam elementum; id tristique
-        lectus convallis? Id erat nec nisl litora vehicula nam curae odio.
-      </Section>
-      <Section sectionName="contact" style={{ marginRight: '59.45vw' }}>
+      <Section sectionName="contact" style={{ marginRight: "59.45vw" }}>
         <p className="section-content">EMAIL = 'tawamzayd@gmail.com'</p>
         <p className="section-content">
           LINKEDIN = '
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,7 +9,7 @@ const Menu = ({ activeSection, scroll }: Props) => {
       <p
         className="menu-item"
         id="menu-about"
-        style={{ color: activeSection === 'about' ? '#000000' : '#9c9c9c' }}
+        style={{ color: activeSection === "about" ? "#000000" : "#9c9c9c" }}
         onClick={() => {
           scroll(0);
         }}
@@ -18,28 +18,30 @@ const Menu = ({ activeSection, scroll }: Props) => {
       </p>
       <p
         className="menu-item"
-        id="menu-projects"
-        style={{ color: activeSection === 'projects' ? '#000000' : '#9c9c9c' }}
+        id="menu-experience"
+        style={{
+          color: activeSection === "experience" ? "#000000" : "#9c9c9c",
+        }}
         onClick={() => {
           scroll(0.405);
+        }}
+      >
+        experience
+      </p>
+      <p
+        className="menu-item"
+        id="menu-projects"
+        style={{ color: activeSection === "projects" ? "#000000" : "#9c9c9c" }}
+        onClick={() => {
+          scroll(0.81);
         }}
       >
         projects
       </p>
       <p
         className="menu-item"
-        id="menu-skills"
-        style={{ color: activeSection === 'skills' ? '#000000' : '#9c9c9c' }}
-        onClick={() => {
-          scroll(0.81);
-        }}
-      >
-        skills
-      </p>
-      <p
-        className="menu-item"
         id="menu-contact"
-        style={{ color: activeSection === 'contact' ? '#000000' : '#9c9c9c' }}
+        style={{ color: activeSection === "contact" ? "#000000" : "#9c9c9c" }}
         onClick={() => {
           scroll(1.215);
         }}
